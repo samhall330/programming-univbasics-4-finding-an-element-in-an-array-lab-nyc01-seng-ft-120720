619db872108e25de43359034a2d9ec0a4623d39b
Sam Hall <mann.samantha11@gmail.com>
2020-11-20 02:16:17 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,11 @@
 def find_element_index(array, value_to_find)
   counter = 0
   while counter < array.length do
-    array.include?(value_to_find)
+    if array.include?(value_to_find)
+    array.index(value_to_find)
+    else
+    nil
     counter += 1
+    
   end
 end
